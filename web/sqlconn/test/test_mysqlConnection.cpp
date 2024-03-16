71fb5916e7dcc692d421443b85fa4763d374aed0
Helens-dot <374961015@qq.com>
2024-03-16 20:41:06 +0800

    change sql

--- web/sqlconn/test/test_mysqlConnection.cpp
+++ web/sqlconn/test/test_mysqlConnection.cpp
@@ -1,33 +1,33 @@
 #include <iostream>
-#include "MySqlConnectionPool.h"
+#include "sqlconn/MySqlConnectionPool.h"
 
-using namespace Tiny_muduo;
 
 int main()
 {
-//    const char *sql =
-//            "DROP table if exists person; "
-//            "create table person (name varchar(256), age int unsigned, weight double) ENGINE=InnoDB  DEFAULT CHARSET=utf8; "
-//            "insert into person(name,age,weight) values('Mary',25,73.8);";
-    std::shared_ptr<MySqlConnection> conn = MySqlConnectionPool::getInstance()->getConnection();
+    const char *sql =
+            "DROP table if exists person; "
+            "create table `person` (`name` varchar(256), `age` int unsigned, `weight` double) ENGINE=InnoDB  DEFAULT CHARSET=utf8; "
+            "insert into person(name,age,weight) values('Mary',25,73.8);";
 
-//    conn->ExecuteNonQuery(sql);
+    std::shared_ptr<SqlConn::MySqlConnection> conn = SqlConn::MySqlConnectionPool::getInstance()->getConnection();
+
+    conn->ExecuteNonQuery(sql);
 
     const char *name = "Rose";
     const char* pass = "24";
 
     auto it = conn->ExecuteNonQuery("insert into user(username,passwd) values(?,?)", name, pass);
-    std::cout << it << std::endl;
-//    int minage = 20;
-//    std::string nm;
-//    double wg;
-//
-//    MySqlDataReader *rd = conn->ExecuteReader("select name,weight from person where age > ?", minage);
-//    while (rd->Read())
-//    {
-//        rd->GetValues(nm, wg);
-//        std::cout << nm << " " << wg << std::endl;
-//    }
-//    delete rd;
+
+    int minage = 20;
+    std::string nm;
+    double wg;
+
+    SqlConn::MySqlDataReader *rd = conn->ExecuteReader("select name,weight from person where age > ?", minage);
+    while (rd->Read())
+    {
+        rd->GetValues(nm, wg);
+        std::cout << nm << " " << wg << std::endl;
+    }
+    delete rd;
     return 0;
 }
